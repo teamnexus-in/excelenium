77874a263f15fc17e9867082471f437ba4a45f57
Prabhu R <prabhu.rengaswamy@outlook.com>
2020-04-06 02:54:20 +0530

    Changed to class structure and clean up of code

--- src/main/resources/static/js/source.js
+++ src/main/resources/static/js/source.js
@@ -1,193 +1,21 @@
-var scriptsCount = 10;
-
-var suiteData = {};
-
-var tabId = 1;
-
-var suite;
+var suiteView;
+var suiteController;
+var suiteModel;
 
 $(document).ready(function () {
 
-    registerBrowserHandlers();
-    registerDialogHandlers();
+    suiteController = new SuiteController();
+    suiteModel = new SuiteModel();
+    suiteView = new SuiteView(suiteController);
 
-    var url = new URL(window.location.href);
-    var qp = url.searchParams.get('type');
-
-    console.log("qp:" + qp);
-    if (qp == "new") {
-        suite = new Suite();
-        suite.initialize();
-        suite.displaySettings();
-        //$('#settingsModal').modal('show');
-        if (scriptsCount == 0) {
-            $('#no-scripts').show();
-        }
-        else {
-            showScriptsTab();
-        }
-
-    }
-    else if (qp == "load") {
-        suite.loadFromFile();
-    }
-
-
+    suiteController.initialize(suiteView, suiteModel);
+    suiteView.initialize(suiteController);
 });
 
-function showScriptsTab() {
-    db = {
-        "actions": ["FILL", "CLEAR", "CLICK", "RIGHT_CLICK", "CHECK", "SELECT", "VERIFY_TEXT", "VERIFY_PRESENT", "IS_VISIBLE", "CHECK_ATTRIBUTE", "ACCEPT_POPUP", "DISMISS_POPUP", "SWITCH_TO_IFRAME", "SWITCH_TO_PARENT", "CAPTURE_SCREEN", "NAVIGATE", "SET_VARIABLE", "UNSET_VARIABLE"
-        ],
-        "preprocess": ["ADD_ATTRIBUTE", "REMOVE_ATTRIBUTE", "SET_ATTRIBUTE"],
-        "scripts": [[]]
-        // "scripts": [
-        //     { "Name": "Script1", "Execute": true, "Stop on Error": true, "Actions": "FILL" },
-        //     { "Name": "Script2", "Execute": true, "Stop on Error": true, "Actions": "VERIFY_TEXT" },
-        //     { "Name": "Script3", "Execute": true, "Stop on Error": true, "Actions": "VERIFY_PRESENT" },
-        //     { "Name": "Script4", "Execute": true, "Stop on Error": true, "Actions": "IS_VISIBLE" },
-        //     { "Name": "Script5", "Execute": true, "Stop on Error": true, "Actions": "SELECT" },
-        //     { "Name": "Script6", "Execute": true, "Stop on Error": true, "Actions": "CLEAR" },
-        //     { "Name": "Script7", "Execute": true, "Stop on Error": true, "Actions": "CHECK_ATTRIBUTE" },
-        //     { "Name": "Script8", "Execute": true, "Stop on Error": true, "Actions": "CHECK_ATTRIBUTE" },
-        //     { "Name": "Script9", "Execute": true, "Stop on Error": true, "Actions": "FILL" }
-        // ]
-    };
-
-    var jxl = $('#scripts-list').jexcel({
-        data: db.scripts,
-        columns: [
-            { type: 'checkbox', title: 'Run', width: 50 },
-            { type: 'checkbox', title: 'Stop on Error', width: 100 },
-            { type: 'text', title: 'Name', width: 150 },
-            { type: 'autocomplete', title: 'Type', width: 150, source: db.actions },
-            { type: 'text', title: 'Id/XPath', width: 250 },
-            { type: 'text', title: 'Element Val', width: 150 },
-            { type: 'text', title: 'Attr Name', width: 150 },
-            { type: 'text', title: 'Attr Val', width: 100 },
-            { type: 'autocomplete', title: 'Action', width: 200, source: db.preprocess },
-            { type: 'text', title: 'Attr Name', width: 100 },
-            { type: 'text', title: 'Attr Val', width: 100 },
-            // { type: 'dropdown', title: 'Actions', width: 200, autocomplete:true, source: db.actions },
-        ],
-        nestedHeaders: [
-            [
-                {
-                    title: 'Actions',
-                    colspan: '8',
-                },
-                {
-                    title: 'PreProcess',
-                    colspan: '3',
-                },
-            ],
-        ]
-    });
-
-    jxl.setRowData(0, ["Script10", true, true, "FILL"]);
-    jxl.insertRow(20);
 
 
-}
 
-function registerBrowserHandlers() {
-    $('.cb-browser-settings').click(function () {
-        var result = $(this).prop('checked') == true;
-        var cboxEl = $(this).attr("id");
-        var idx = cboxEl.indexOf('-');
-        var elName = cboxEl.substr(idx + 1);
-        console.log(cboxEl + " " + elName);
-        var tbEl = '#tb-' + elName
-        $(tbEl).attr('disabled', !result);
-        // $("#tb-chrome").attr('disabled', !result);
-    });
-    // $('#cb-firefox').click(function () {
-    //     var result = $(this).prop('checked') == true;
-    //     $("#tb-firefox").attr('disabled', !result);
-    // });
-    // $('#cb-edge').click(function () {
-    //     var result = $(this).prop('checked') == true;
-    //     $("#tb-edge").attr('disabled', !result);
-    // });
-    // $('#cb-opera').click(function () {
-    //     var result = $(this).prop('checked') == true;
-    //     $("#tb-opera").attr('disabled', !result);
-    // });
-    // $('#cb-user-agent').click(function () {
-    //     var result = $(this).prop('checked') == true;
-    //     $("#tb-user-agent").attr('disabled', !result);
-    // });
-}
 
-function registerDialogHandlers() {
-    // Save Suite Settingss
-    $('#btn-settings-save').click(function () {
-        suite.saveSettings();
-    });
 
-    // Create New Script
-    $('#btn-new-script-ok').click(function () {
-        var scriptName = $('#tb-script-name').val();
-        if (scriptName != '') {
-            createNewScript(scriptName);
-            $("#newScriptModal").modal('hide');
-        }
-    });
 
-    $("#btn-suite-new").click(function () {
-        // check if a script is already open and save it before creating a new one
-        clearSettingsFields();
-        $("#settingsModal").modal('show');
-    });
 
-    $('#btn-settings').click(function () {
-        // load the settings into the fields and show
-        suite.displaySettings();
-    });
-
-    $('#btn-new-script').click(function () {
-        clearNewScriptDialog();
-        $("#newScriptModal").modal('show');
-    });
-
-}
-
-function clearNewScriptDialog() {
-    $("#tb-script-name").val('');
-    $('cb-ns-stop-on-error').prop('checked', false);
-    $('cb-ns-execute').prop('checked', false);
-}
-
-function clearSettingsFields() {
-    console.log("Clearing settings for new suite");
-    $('.cb-browser-settings').each(function () {
-        console.log("Before: " + $(this).is(':checked'))
-        $(this).prop('checked', false);
-        console.log("After: " + $(this).is(':checked'))
-    });
-    $('.tb-browser-settings').each(function () {
-        $(this).val('');
-    });
-
-    $('#tb-suite-name').val('');
-    $('#tb-server-url').val('');
-    $('#cb-run-browsers-concurrent').prop('checked', false);
-}
-
-function createNewScript(scriptName) {
-    // Add a new tab
-    createNewTab(scriptName);
-    // register event handler
-    // create sheet
-    // add row in scripts tab
-}
-
-function createNewTab(scriptName) {
-    tabId++
-    $('#suite-tabs').append('<li class="nav-item"><a class="nav-link" id="tab-scripts' + tabId + '" data-toggle="tab" href="#tpanel-scripts-tab' + tabId + '" role="tab" aria-controls="scripts" aria-selected="true">' + scriptName + '</a></li>');
-    $('#scripts-tab-content').append('<div class="tab-pane fade show" id="tpanel-scripts-tab' + tabId + '" role="tabpanel" aria-labelledby="scripts-tab' + tabId + '"><div id="scripts' + tabId + '" class="sheets-content">New content of Tab ' + tabId + '</div></div>')
-}
-
-function saveSuiteSettings() {
-    console.log("Saved Suite Settings");
-}
--- src/main/resources/static/js/suite.js
+++ src/main/resources/static/js/suite.js
@@ -1,41 +1,204 @@
-function Suite() {
-    var scriptCount;
+var actions = {
+    "main": ["FILL", "CLEAR", "CLICK", "RIGHT_CLICK", "CHECK", "SELECT", "VERIFY_TEXT", "VERIFY_PRESENT", "IS_VISIBLE", "CHECK_ATTRIBUTE", "ACCEPT_POPUP", "DISMISS_POPUP", "SWITCH_TO_IFRAME", "SWITCH_TO_PARENT", "CAPTURE_SCREEN", "NAVIGATE", "SET_VARIABLE", "UNSET_VARIABLE"
+    ],
+    "preprocess": ["ADD_ATTRIBUTE", "REMOVE_ATTRIBUTE", "SET_ATTRIBUTE"],
+};
+
+
+/// ================ SuiteModel =========================
+class SuiteModel {
+    constructor() {
+    }
+    getJson() {
+    }
+    addNewScript(scriptName) {
+    }
 }
 
-Suite.prototype.initialize = function(){
-    this.scriptCount = 0;
 
-    // Clear the settings fields
-    console.log("Clearing settings for new suite");
-    $('.cb-browser-settings').each(function () {
-        $(this).prop('checked', false);
-    });
-    $('.tb-browser-settings').each(function () {
-        $(this).val('');
-    });
+/// ================ SuiteController =========================
+class SuiteController {
+    constructor() {
+    }
 
-    $('#tb-suite-name').val('');
-    $('#tb-server-url').val('');
-    $('#cb-run-browsers-concurrent').prop('checked', false);
+    initialize(suiteView, suiteModel) {
+        this.suiteView = suiteView;
+        this.suiteModel = suiteModel;
+    }
+
+    saveSettings() {
+        console.log("Saving settings");
+    }
+
+    createNewScript(scriptName) {
+        this.suiteModel.addNewScript(scriptName);
+        this.suiteView.hideNewScriptDialog();
+        this.suiteView.createNewScriptTab(scriptName);
+    }
+
+    closeSuite() {
+    }
 }
 
-Suite.prototype.displaySettings = function(){
-    // Show the settings modal
-    $('#settingsModal').modal('show');
+
+/// ================ SuiteView =========================
+class SuiteView {
+    constructor() {
+        this.data = { "scripts": {} };
+        this.tabIdx = 0;
+    }
+
+    initialize(controller) {
+        this.controller = controller;
+        this.registerHandlers();
+    }
+
+    registerHandlers() {
+        let thisViewObj = this;
+        $('.cb-browser-settings').click(function () {
+            var result = ($(this).prop('checked') == true);
+            var cboxEl = $(this).attr("id");
+            var idx = cboxEl.indexOf('-');
+            var elName = cboxEl.substr(idx + 1);
+            console.log(cboxEl + " " + elName);
+            var tbEl = '#tb-' + elName;
+            $(tbEl).attr('disabled', !result);
+            // $("#tb-chrome").attr('disabled', !result);
+        });
+        $('#btn-settings-save').click(function () {
+            thisViewObj.controller.saveSettings();
+        });
+        // Create New Script
+        $('#btn-new-script-ok').click(function () {
+            var scriptName = $('#tb-script-name').val();
+            console.log(thisViewObj);
+            thisViewObj.controller.createNewScript(scriptName);
+        });
+        $("#btn-suite-new").click(function () {
+            // check if a script is already open and save it before creating a new one
+            thisViewObj.showNewSuiteWarning();
+            thisViewObj.displaySettings(true);
+        });
+        $('#btn-settings').click(function () {
+            // load the settings into the fields and show
+            thisViewObj.displaySettings(false);
+        });
+        $('#btn-new-script').click(function () {
+            thisViewObj.clearNewScriptDialogFields();
+            thisViewObj.showNewScriptDialog();
+        });
+    }
+
+    showNewSuiteWarning() {
+        console.log("Showing new suite warning");
+    }
+
+    showNewScriptDialog() {
+        $("#newScriptModal").modal('show');
+    }
+
+    hideNewScriptDialog() {
+        $("#newScriptModal").modal('hide');
+    }
+
+    createNewScriptTab(scriptName) {
+        this.tabIdx++;
+        let tabId = 'tab-scripts-' + this.tabIdx;
+        let panelId = "tpanel-scripts-tab-" + this.tabIdx;
+        let sheetsId = "sheets-" + this.tabIdx;
+        if (this.data.scripts[scriptName] == undefined) {
+            this.data.scripts[scriptName] = {};
+        }
+        console.log(this.data);
+        let scriptData = this.data.scripts[scriptName];
+        scriptData.tabIdx = this.tabIdx;
+        scriptData.tabId = tabId;
+        scriptData.panelId = panelId;
+        scriptData.sheetsId = sheetsId;
+
+        $('#suite-tabs').append('<li class="nav-item"><a class="nav-link" id="' + tabId + '" data-toggle="tab" href="#' + panelId + '" role="tab" aria-controls="scripts" aria-selected="true">' + scriptName + '</a></li>');
+        $('#scripts-tab-content').append('<div class="tab-pane fade show" id="' + panelId + '" role="tabpanel" aria-labelledby="' + tabId + '"><div id="' + sheetsId + '" class="sheets-content"></div></div>');
+
+        let jxl = this.createSheets(sheetsId);
+        scriptData.jxl = jxl;
+        this.addScriptsTabEntry(scriptName);
+        $('#'+tabId).tab('show');
+    }
+
+    createSheets(sheetsId) {
+        let jxl = $('#' + sheetsId).jexcel({
+            data: [[]],
+            defaultColWidth: 100,
+            tableOverflow: true,
+            tableWidth: "100%",
+            tableHeight: "100%",
+            columns: [
+                { type: 'checkbox', title: 'Run', width: 50 },
+                { type: 'checkbox', title: 'Stop on Error', width: 100 },
+                { type: 'text', title: 'Name', width: 150 },
+                { type: 'autocomplete', title: 'Type', width: 150, source: actions.main },
+                { type: 'text', title: 'Id/XPath', width: 250 },
+                { type: 'text', title: 'Element Val', width: 150 },
+                { type: 'text', title: 'Attr Name', width: 150 },
+                { type: 'text', title: 'Attr Val', width: 100 },
+                { type: 'autocomplete', title: 'Action', width: 200, source: actions.preprocess },
+                { type: 'text', title: 'Attr Name', width: 100 },
+                { type: 'text', title: 'Attr Val', width: 100 },
+                // { type: 'dropdown', title: 'Actions', width: 200, autocomplete:true, source: db.actions },
+            ],
+            nestedHeaders: [
+                [
+                    {
+                        title: 'Actions',
+                        colspan: '8',
+                    },
+                    {
+                        title: 'PreProcess',
+                        colspan: '3',
+                    },
+                ],
+            ]
+        });
+        jxl.insertRow(19);
+
+        return jxl;
+    }
+
+    displaySettings(isClearSettings) {
+        if (isClearSettings) {
+            this.clearSettingsFields();
+        }
+        $("#settingsModal").modal('show');
+    }
+
+    clearSettingsFields() {
+        console.log("Clearing settings for new suite");
+        $('.cb-browser-settings').each(function () {
+            console.log("Before: " + $(this).is(':checked'));
+            $(this).prop('checked', false);
+            console.log("After: " + $(this).is(':checked'));
+        });
+        $('.tb-browser-settings').each(function () {
+            $(this).val('');
+        });
+        $('#tb-suite-name').val('');
+        $('#tb-server-url').val('');
+        $('#cb-run-browsers-concurrent').prop('checked', false);
+    }
+
+    clearNewScriptDialogFields() {
+        $("#tb-script-name").val('');
+        $('cb-ns-stop-on-error').prop('checked', false);
+        $('cb-ns-execute').prop('checked', false);
+    }
 }
 
-Suite.prototype.saveSettings = function(){
-    console.log("Saving the settings");
-}
 
-Suite.prototype.save = function(){
 
-}
 
-Suite.prototype.addNewScript = function(){
 
-}
 
-Suite.prototype.close = function(){
 
-}
+
+
+
